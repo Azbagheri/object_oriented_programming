7f09e45be9c739702fc123ee60aa8f42de312662
Azbagheri <azadeh.bagheri7@gmail.com>
2017-03-29 16:47:43 -0400

    Part 5, 6 of the cat exercise is added

--- the-cats-meow/cat.rb
+++ the-cats-meow/cat.rb
@@ -7,19 +7,29 @@ class  Cat
            @meal_time = meal_time
    end
 
-   Zooloo = Cat.new("Zoozoo", "Salmon", 10)
-   Tifa = Cat.new("Tifa", "Turkey", 21)
+
 
    def eats_at
 
-      if (@meal_time <= 11) && (@meal_time >= 0)
+      if (@meal_time <= 11) && (@meal_time > 0)
         time = @meal_time.to_s + " AM"
+      elsif @meal_time == 0
+        time = "12 AM"
       else
         time = @meal_time.to_s + " PM"
       end
       return time
    end
 
-
+    def meow
+      puts "My name is #{self.name}, and I eat #{self.preferred_food} at #{self.eats_at}."
+    end
 
 end
+
+
+zooloo = Cat.new("Zooloo", "Salmon", 10)
+tifa = Cat.new("Tifa", "Turkey", 21)
+
+puts zooloo.meow
+puts tifa.meow
