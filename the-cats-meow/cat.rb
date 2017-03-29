723e28f14d70d2f32feeff6d420162811e92f5f7
Azbagheri <azadeh.bagheri7@gmail.com>
2017-03-29 16:07:02 -0400

    part 3 and 4 of the cat exercise added

--- the-cats-meow/cat.rb
+++ the-cats-meow/cat.rb
@@ -7,7 +7,18 @@ class  Cat
            @meal_time = meal_time
    end
 
+   Zooloo = Cat.new("Zoozoo", "Salmon", 10)
+   Tifa = Cat.new("Tifa", "Turkey", 21)
 
+   def eats_at
+
+      if (@meal_time <= 11) && (@meal_time >= 0)
+        time = @meal_time.to_s + " AM"
+      else
+        time = @meal_time.to_s + " PM"
+      end
+      return time
+   end
 
 
 
