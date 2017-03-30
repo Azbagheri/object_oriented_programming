ca33c7a6f343125d0472687f73f9fea25e3b1f75
Azbagheri <azadeh.bagheri7@gmail.com>
2017-03-30 14:11:07 -0400

    method deliver is added to paperboy

--- paperboy/paperboy.rb
+++ paperboy/paperboy.rb
@@ -2,19 +2,25 @@ class Paperboy
 
 attr_reader :name, :experience, :earnings
 
-def initialize(name, experience, earnings)
+def initialize(name)
   @name = name
-  @experience = experience
-  @earnings = earnings
+  @experience = 0
+  @earnings = 0
 end
 
 def quota
   base = 50
   quota = base + @experience/2
-  "Quota is #{quota}"
 end
 
-
+def deliver(start_address, end_address)
+  num_delivered = (end_address - start_address) + 1
+  over_quota = num_delivered - self.quota
+  base_pay = 0.25
+  extra_pay = 0.50
+  @earnings = num_delivered * base_pay + over_quota * extra_pay
+  @experience = num_delivered
+end
 
 
 
